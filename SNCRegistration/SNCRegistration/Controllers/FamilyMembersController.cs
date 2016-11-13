226bf1035eb0a22a11e86fb1c589822f56c854d6
Jennifer Stelmach <jstelmach@klsmartin.com>
2016-11-13 09:23:02 -0500

    JS: family members controller & views.  Family 'create' view formatted.

---
+++ SNCRegistration/SNCRegistration/Controllers/FamilyMembersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SNCRegistration.ViewModels;
+
+namespace SNCRegistration.Controllers
+{
+    public class FamilyMembersController : Controller
+    {
+        private SNCRegistrationEntities db = new SNCRegistrationEntities();
+
+        // GET: FamilyMembers
+        public ActionResult Index()
+        {
+            return View(db.FamilyMembers.ToList());
+        }
+
+        // GET: FamilyMembers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FamilyMember familyMember = db.FamilyMembers.Find(id);
+            if (familyMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(familyMember);
+        }
+
+        // GET: FamilyMembers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FamilyMembers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FamilyMemberID,FamilyMemberFirstName,FamilyMemberLastName,GuardianID,HealthForm,PhotoAck,AttendingCode,Comments")] FamilyMember familyMember)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FamilyMembers.Add(familyMember);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(familyMember);
+        }
+
+        // GET: FamilyMembers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FamilyMember familyMember = db.FamilyMembers.Find(id);
+            if (familyMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(familyMember);
+        }
+
+        // POST: FamilyMembers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FamilyMemberID,FamilyMemberFirstName,FamilyMemberLastName,GuardianID,HealthForm,PhotoAck,AttendingCode,Comments")] FamilyMember familyMember)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(familyMember).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(familyMember);
+        }
+
+        // GET: FamilyMembers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FamilyMember familyMember = db.FamilyMembers.Find(id);
+            if (familyMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(familyMember);
+        }
+
+        // POST: FamilyMembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FamilyMember familyMember = db.FamilyMembers.Find(id);
+            db.FamilyMembers.Remove(familyMember);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
