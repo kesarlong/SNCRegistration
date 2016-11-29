c538e951e828f5623f0208f2c8b8ab014372f542
Erika Hilty <erika.hilty@yahoo.com>
2016-11-29 16:27:38 -0500

    Added LeadContact and Volunteer Forms
    Added LeadContact and Volunteer Forms

--- SNCRegistration/SNCRegistration/ViewModels/SNCRegistration.Designer.cs
+++ SNCRegistration/SNCRegistration/ViewModels/SNCRegistration.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\kesar\Documents\Visual Studio 2015\Projects\SNCRegistration\SNCRegistration\SNCRegistration\ViewModels\SNCRegistration.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Erika\Source\Repos\SNCRegistration\SNCRegistration\SNCRegistration\SNCRegistration\ViewModels\SNCRegistration.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
