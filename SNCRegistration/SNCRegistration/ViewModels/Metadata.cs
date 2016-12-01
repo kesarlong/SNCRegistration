63d9ee59fb9fb7940cc66f42b12c055d99df1a3d
Erika Hilty <erika.hilty@yahoo.com>
2016-11-30 20:17:37 -0500

    Changes New Branch

--- SNCRegistration/SNCRegistration/ViewModels/Metadata.cs
+++ SNCRegistration/SNCRegistration/ViewModels/Metadata.cs
@@ -158,4 +158,122 @@ namespace SNCRegistration.ViewModels.Metadata
 
         
     }
-}
+
+    public class LeadContact_Metadata
+    {
+  
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LeadContactID { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string LeadContactFirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LeadContactLastName { get; set; }
+
+        [Required]
+        [DisplayName("Street Address")]
+        public string LeadContactAddress { get; set; }
+
+        [Required]
+        [DisplayName("City")]
+        public string LeadContactCity { get; set; }
+
+        [Required]
+        public string LeadContactState { get; set; }
+
+        [Required]
+        [DisplayName("Zip Code")]
+        public string LeadContactZip { get; set; }
+
+        [Required]
+        [DisplayName("Cell Phone")]
+        [DataType(DataType.PhoneNumber)]
+        public string LeadContactCellPhone { get; set; }
+
+        [Required]
+        [DisplayName("Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string LeadContactEmail { get; set; }
+
+        [Required]
+        [DisplayName("T-Shirt Order")]
+        public bool LeadContactShirtOrder { get; set; }
+
+        [Required]
+        [DisplayName("T-Shirt Size")]
+        public string LeadContactShirtSize { get; set; }
+
+        [Required]
+        [DisplayName("Troop/Chapter/Unit")]
+        public string BSType { get; set; }
+
+        [Required]
+        [DisplayName("Troop/Chapter/Unit Number")]
+        public string UnitChapterNumber { get; set; }
+
+        [Required]
+        [DisplayName("Attending")]
+        public string VolunteerAttendingCode { get; set; }
+
+        [Required]
+        [DisplayName("Saturday Dinner?")]
+        public bool SaturdayDinner { get; set; }
+
+        [Required]
+        [DisplayName("Booth Name")]
+        public string Booth { get; set; }
+
+        [Required]
+        [DisplayName("Comments")]
+        public string Comments { get; set; }
+
+
+        public Decimal TotalFee { get; set; }
+
+    }
+        public class Volunteer_Metadata
+        {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VolunteerID { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string VolunteerFirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string VolunteerLastName { get; set; }
+
+        [DisplayName("Age")]
+        public string VolunteerAge { get; set; }
+
+        [DisplayName("T-Shirt Order")]
+        public Nullable<bool> VolunteerShirtOrder { get; set; }
+
+        [DisplayName("T-Shirt Size")]
+        public string VolunteerShirtSize { get; set; }
+
+        [DisplayName("Attending")]
+        public string VolunteerAttendingCode { get; set; }
+
+        [DisplayName("Saturday Dinner?")]
+        public Nullable<bool> SaturdayDinner { get; set; }
+
+        [DisplayName("Troop/Chapter/Unit Number")]
+        public string UnitChapterNumber { get; set; }
+
+        [DisplayName("Comments")]
+        public string Comments { get; set; }
+
+        public Nullable<int> LeadContactID { get; set; }
+
+        public virtual Volunteer Volunteers1 { get; set; }
+
+        public virtual Volunteer Volunteer1 { get; set; }
+
+    }
+    }
