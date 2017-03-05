18a1635fed496fdc6eeb31f4b82e3d4a1d94c995
Kesar Long <kesarlong@outlook.com>
2017-03-05 14:41:29 -0500

    Merge with  local reporting

---
+++ SNCRegistration/Controllers/ParticipantsReportController.cs
@@ -0,0 +1,94 @@
+﻿using ClosedXML.Excel;
+using SNCRegistration.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.Mvc;
+using PagedList;
+
+namespace SNCRegistration.Controllers
+    {
+    public class ParticipantsReportController : Controller
+        {
+     
+        // GET: Reporting
+        public ActionResult Index()
+            {
+            string constring = ConfigurationManager.ConnectionStrings["ReportConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(constring);
+            string query = "SELECT * FROM Participants INNER JOIN Age ON ParticipantAge = AgeID";
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            da.Fill(dt);
+            con.Close();
+            IList<ParticipantsReportModel> model = new List<ParticipantsReportModel>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                model.Add(new ParticipantsReportModel()
+                    {
+                    ParticipantID = Convert.ToInt32(dt.Rows[i]["ParticipantID"]),
+                    ParticipantFirstName = dt.Rows[i]["ParticipantFirstName"].ToString(),
+                    ParticipantLastName = dt.Rows[i]["ParticipantLastName"].ToString(),
+                    });
+                }
+            return View(model);
+            }
+
+        public ActionResult ParticipantsReport()
+            {
+            string constring = ConfigurationManager.ConnectionStrings["ReportConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(constring);
+            string query = "SELECT * FROM Participants INNER JOIN Age ON ParticipantAge = AgeID";
+            DataTable dt = new DataTable();
+            dt.TableName = "Participants";
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            da.Fill(dt);
+            con.Close();
+
+            using (XLWorkbook wb = new XLWorkbook())
+                {
+                wb.Worksheets.Add(dt);
+                wb.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                wb.Style.Font.Bold = true;
+
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;filename= ParticipantsReport.xlsx");
+
+                using (MemoryStream MyMemoryStream = new MemoryStream())
+                    {
+                    wb.SaveAs(MyMemoryStream);
+                    MyMemoryStream.WriteTo(Response.OutputStream);
+                    Response.Flush();
+                    Response.End();
+                    }
+                }
+
+            return RedirectToAction("Index", "ParticipantsReport");
+            }
+
+        private void releaseObject(object obj)
+            {
+            try
+                {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+                }
+            catch
+                {
+                obj = null;
+                }
+            finally
+                {
+                GC.Collect();
+                }
+            }
+        }
+    }
---
+++ SNCRegistration/ViewModels/BoothCountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SNCRegistration.ViewModels
+    {
+    public class BoothCountModel
+        {
+       
+        [Display(Name = "Booth")]
+        public string Booth { get; set; }
+        }
+    }
