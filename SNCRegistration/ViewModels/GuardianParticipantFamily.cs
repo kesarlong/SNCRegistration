eba5dfc8d2eb808fa055b6166add63f74ddbaece
Einar <Einar.Acuna@gmail.com>
2017-03-16 18:00:12 -0400

    Updates

---
+++ SNCRegistration/ViewModels/GuardianParticipantFamily.cs
@@ -0,0 +1,21 @@
+﻿using SNCRegistration.ViewModels.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SNCRegistration.ViewModels
+{
+    public class GuardianParticipantFamily
+    {
+        public Guardian Guardians { get; set; }
+        public Participant Participants { get; set; }
+        public FamilyMember FamilyMembers { get; set; }
+
+        //public IEnumerable<Guardian> Guardians { get; set; }
+        //public IEnumerable<Participant> Participant { get; set; }
+
+        //public IEnumerable<FamilyMember> FamilyMember { get; set; }
+    }
+}
