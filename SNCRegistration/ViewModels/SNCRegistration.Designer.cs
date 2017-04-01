ebbc8ed037e3248d21965f6d56059ffaff4e9599
Einar <Einar.Acuna@gmail.com>
2017-03-31 22:36:48 -0400

    Updates to EDMX, and .NetCompiler to ver 2

--- SNCRegistration/ViewModels/SNCRegistration.Designer.cs
+++ SNCRegistration/ViewModels/SNCRegistration.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\jstelmach\Documents\GitHub\SNCRegistration\SNCRegistration\ViewModels\SNCRegistration.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Einar\Source\Repos\SNCRegistration\SNCRegistration\ViewModels\SNCRegistration.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
